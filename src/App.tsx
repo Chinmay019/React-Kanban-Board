73474927c700345241d6f2396a0089ddb4175ca5
Chinmay019 <chinmaynaik019@gmail.com>
2024-02-18 00:30:55 +0530

    code cleanup in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css';
 import Board from './components/board/Board';
 import Navbar from './components/navbar/Navbar';
-// import { TasksProvider } from './context/TasksContext';
 
 function App () {
   return (
