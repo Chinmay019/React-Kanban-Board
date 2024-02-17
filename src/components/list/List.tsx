af678967a90739364a6e9279c8665e7593134856
Chinmay019 <chinmaynaik019@gmail.com>
2024-02-18 00:20:15 +0530

    code cleanup in list component

--- src/components/list/List.tsx
+++ src/components/list/List.tsx
@@ -12,72 +12,6 @@ interface BoardProps {
 
 
 export const List = (props: BoardProps) => {
-    // const ItemList = props.items;
-    // const [isModified, setIsModified] = useState(false);
-    // const [todoTasks, setToDoTasks] = useState<ItemProps[] | null>(null);
-    // const [doingTasks, setDoingTasks] = useState<ItemProps[] | null>(null);
-    // const [doneTasks, setDoneTasks] = useState<ItemProps[] | null>(null);
-    // let itemDragged: EventTarget;
-    // useEffect(() => {
-    //     // setToDoTasks(fetchFilteredTasks("1"));
-    //     // setDoingTasks(fetchFilteredTasks("2"));
-    //     // setDoneTasks(fetchFilteredTasks("3"));
-    //     console.log("inside useEffect");
-    //     console.log(ItemList);
-    //     return () => {
-    //         console.log("inside cleanup");
-    //         setIsModified(false);
-    //     }
-    // }, [isModified]);
-
-    // const removeElement = (id: string) => {
-    //     console.log(id);
-    //     const elem = document.getElementById(id);
-    //     console.log(elem);
-    //     return elem?.parentNode?.removeChild(elem);
-    // }
-
-    // const handleOnDragOver = (e: React.DragEvent) => {
-    //     e.preventDefault();
-    // }
-
-    // const handleOnDrag = (e: React.DragEvent, id: string) => {
-    //     console.log("dragging started");
-    //     console.log(id);
-    //     // itemDragged = e.target;
-    //     e.dataTransfer.setData("id", id);
-    // }
-
-    // const handleOnDrop = (e: React.DragEvent, targetCategory: string) => {
-    //     const draggedElementId = e.dataTransfer.getData("id") as string;
-    //     ItemList.forEach(item => {
-    //         if (item.id == draggedElementId && item.category === targetCategory) {
-    //             return;
-    //         } else if (item.id == draggedElementId && item.category != targetCategory) {
-    //             const classes = e.target.className;
-    //             if (classes.includes("dropzone")) {
-    //                 item.category = targetCategory;
-    //                 removeElement(draggedElementId);
-    //                 // setIsModified(true);
-    //             }
-    //         }
-    //     })
-    // }
-
-    // const deleteTask = (taskId: string) => {
-    //     removeElement(taskId);
-    //     // setIsModified(true);
-    // }
-
-    // const fetchFilteredTasks = (cat: string): ItemProps[] | null => {
-    //     const items = ItemList?.filter(item => {
-    //         if (item.category == cat) {
-    //             return item;
-    //         }
-    //     });
-    //     return items;
-    // }
-
     return (
         <div className="overflow-auto">
             <div className="container">
@@ -106,23 +40,6 @@ export const List = (props: BoardProps) => {
                             } }
                         </Droppable>
                     </div>
-                    {/* <div className="card-body dropzone" onDragOver={ handleOnDragOver } onDrop={ (e) => handleOnDrop(e, props.category) }>
-                        { props.items?.map(task => (
-                            <div onDragStart={ (e) => handleOnDrag(e, task.id) } onDragOver={ handleOnDragOver }>
-                                <Item key={ task.id } item={ task } deleteTask={ deleteTask } />
-                            </div>
-                        )) }
-                        {/* { doingTasks?.map(task => (
-                            <div onDragStart={ (e) => handleOnDrag(e, task.id) } onDragOver={ handleOnDragOver }>
-                                <Item key={ task.id } item={ task } deleteTask={ deleteTask } />
-                            </div>
-                        )) }
-                        { props.category == "3" && doneTasks?.map(task => (
-                            <div onDragStart={ (e) => handleOnDrag(e, task.id) } onDragOver={ handleOnDragOver }>
-                                <Item key={ task.id } item={ task } deleteTask={ deleteTask } />
-                            </div>
-                        )) }
-                    </div> */}
                 </div>
             </div>
         </div>
